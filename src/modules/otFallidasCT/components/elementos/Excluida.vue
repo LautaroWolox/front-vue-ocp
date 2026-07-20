34380be672f22b3b673278375fe6616ccfe2e1c4
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-20 16:20:53 -0300

    refactor: move excluded filter UI into element component

--- src/modules/otFallidasCT/components/elementos/Excluida.vue
+++ src/modules/otFallidasCT/components/elementos/Excluida.vue
@@ -1,31 +1,41 @@
 <template>
-    <div class="flex flex-column px-2">
-        <label for="excl">Excluida</label>
-        <Select id="excl" v-model="exc" :options="excluida" optionLabel="valor"class="w-full md:w-56" />
-    </div>
+  <div
+    class="fm-field fm-field--span-3 otf-filter-element otf-filter-element--excluida"
+    :class="{ 'otf-filter-element--disabled': disabled }"
+  >
+    <label for="otf-excluida">Excluida</label>
+    <Select
+      inputId="otf-excluida"
+      v-model="excluida"
+      :options="options"
+      optionLabel="label"
+      optionValue="value"
+      :disabled="disabled"
+      class="w-full"
+    />
+  </div>
 </template>
 
 <script setup>
-import { ref, computed } from "vue";
+import { computed } from 'vue'
+import Select from 'primevue/select'
 import { useFallidasCtStore } from '../../store/CtFallidaStore'
+import './filter-element.css'
+
+defineProps({
+  disabled: { type: Boolean, default: false }
+})
 
 const store = useFallidasCtStore()
 
-const excluida = ref ([
-    {'id': '', 'valor': ''},
-    {'id': 'S', 'valor': 'SI'},
-    {'id': 'N', 'valor': 'NO'},
-])
+const options = [
+  { label: '', value: '' },
+  { label: 'SI', value: 'S' },
+  { label: 'NO', value: 'N' }
+]
 
-const exc = computed({
-  get: () =>
-    excluida.value.find(p => p.valor === store.filters.excluida) ?? exc.value[0],
-
-  set: (value) =>
-    store.setFilter('excluida', value?.id ?? '')
+const excluida = computed({
+  get: () => store.filters.excluida,
+  set: (value) => store.setFilter('excluida', value ?? '')
 })
-
 </script>
-
-<style scoped>
-</style>
