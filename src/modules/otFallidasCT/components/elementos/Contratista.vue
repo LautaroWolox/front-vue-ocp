c3d264d942afe0b58d58c817326df176106adc95
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-20 16:20:36 -0300

    refactor: move contractor filter UI into element component

--- src/modules/otFallidasCT/components/elementos/Contratista.vue
+++ src/modules/otFallidasCT/components/elementos/Contratista.vue
@@ -1,62 +1,52 @@
 <template>
-    <div class="flex flex-column px-2">
-        <label for="desc">Contratista</label>       
-        <span v-if="status.contratistas === 'loading'">
-            Cargando...
-        </span>
-        <Select
-            v-else-if="status.contratistas === 'loaded'"
-            v-model="contratista"
-            :options="contratistaOptions"
-            optionLabel="nombre"
-        />
-        <span v-else-if="status.contratistas === 'error'">
-            Error al cargar.
-        </span>
-    </div>
+  <div
+    class="fm-field fm-field--span-3 otf-filter-element otf-filter-element--contratista"
+    :class="{ 'otf-filter-element--disabled': fieldDisabled }"
+  >
+    <label for="otf-contratista">Contratista</label>
+    <Select
+      inputId="otf-contratista"
+      v-model="contratistaSelected"
+      :options="contratistaOptions"
+      optionLabel="nombre"
+      :loading="status.contratistas === 'loading'"
+      :disabled="fieldDisabled"
+      class="w-full"
+      showClear
+    />
+    <span v-if="status.contratistas === 'error'" class="otf-filter-element__error">
+      Error al cargar contratistas.
+    </span>
+  </div>
 </template>
 
 <script setup>
-import { useFallidasCtStore } from '../../store/CtFallidaStore'
-import { ref, computed, onMounted } from "vue";
+import { computed, onMounted } from 'vue'
 import { storeToRefs } from 'pinia'
+import Select from 'primevue/select'
+import { useFallidasCtStore } from '../../store/CtFallidaStore'
 import { useCommonCtStore } from '@/store/commonCt'
+import './filter-element.css'
+
+const props = defineProps({
+  disabled: { type: Boolean, default: false }
+})
 
 const store = useFallidasCtStore()
 const commonCT = useCommonCtStore()
 const { contratistas, status } = storeToRefs(commonCT)
 
-const contratista = computed({
-  get: () => {
-    if (!store.filters.contratista) {
-      return contratistaOptions.value[0]
-    }
-    return (
-      contratistaOptions.value.find(
-        item => item.codigo === store.filters.contratista
-      ) 
-    )
-  },
-  set: value => {
-    store.setFilter('contratista', value?.codigo ?? '')
-  },
-})
+const fieldDisabled = computed(() => props.disabled || status.value.contratistas === 'loading')
 
-const contratistaOptions = computed (() => [
-  {
-    empresaId: 0,
-    codigo: '',
-    nombre: '',
-    tipo: 'Contratista',
-    activo: 'S',
-  },
-  ...(contratistas.value ?? []),
+const contratistaOptions = computed(() => [
+  { empresaId: 0, codigo: '', nombre: '', tipo: 'Contratista', activo: 'S' },
+  ...(contratistas.value ?? [])
 ])
 
+const contratistaSelected = computed({
+  get: () => contratistaOptions.value.find((item) => item.codigo === store.filters.contratista) ?? null,
+  set: (value) => store.setFilter('contratista', value?.codigo ?? '')
+})
+
 onMounted(() => commonCT.setContratistas())
-
 </script>
-
-<style scoped>
-</style>
-
