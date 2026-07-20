206da3fea56c530b3136e92a07b35e87f87d6dd6
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-20 16:19:23 -0300

    refactor: move OT number filter UI into element component

--- src/modules/otFallidasCT/components/elementos/NroOT.vue
+++ src/modules/otFallidasCT/components/elementos/NroOT.vue
@@ -1,25 +1,34 @@
 <template>
-    <div class="flex flex-column px-2">
-        <label for="ot">Nro OT</label>
-        <InputText type="text" id="ot" v-model="nroOt"/>
-    </div>
+  <div
+    class="fm-field fm-field--span-3 otf-filter-element otf-filter-element--nro-ot"
+    :class="{ 'otf-filter-element--disabled': disabled }"
+  >
+    <label for="otf-nro-ot">Nro. OT</label>
+    <InputText
+      id="otf-nro-ot"
+      v-model="nroOt"
+      :disabled="disabled"
+      class="w-full"
+      type="text"
+      autocomplete="off"
+    />
+  </div>
 </template>
 
-
 <script setup>
-import { computed } from 'vue';
-import InputText from 'primevue/inputtext';
-import { useFallidasCtStore } from '../../store/CtFallidaStore';
+import { computed } from 'vue'
+import InputText from 'primevue/inputtext'
+import { useFallidasCtStore } from '../../store/CtFallidaStore'
+import './filter-element.css'
+
+const props = defineProps({
+  disabled: { type: Boolean, default: false }
+})
 
 const store = useFallidasCtStore()
 
 const nroOt = computed({
   get: () => store.filters.nroOT,
-  set: (nroOt) =>
-    store.setFilter('nroOT', nroOt? nroOt : '') 
+  set: (value) => store.setFilter('nroOT', value ?? '')
 })
-
 </script>
-
-<style scoped>
-</style>
