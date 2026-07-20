a3057f378bbd6d488e0c9adc15046f6dc489ed3b
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-20 16:20:16 -0300

    refactor: move compact to-date filter into element component

--- src/modules/otFallidasCT/components/elementos/FechaHasta.vue
+++ src/modules/otFallidasCT/components/elementos/FechaHasta.vue
@@ -1,30 +1,32 @@
 <template>
-    <div class="flex flex-column px-2">
-        <label for="date">Fecha Hasta</label>
-        <DatePicker id="date" dateFormat="dd/mm/yy" :manualInput="false" class="w-auto"
-        v-model="fechaHasta" variant="filled" @clear-click="borrarFecha($event)" showIcon 
-        selectOtherMonths showButtonBar />
-    </div>
+  <div
+    class="fm-field fm-field--span-3 otf-filter-element otf-filter-element--fecha-hasta"
+    :class="{ 'otf-filter-element--disabled': disabled }"
+  >
+    <label for="otf-fecha-hasta">Fecha hasta</label>
+    <CtDatePicker
+      v-model="fechaHasta"
+      inputId="otf-fecha-hasta"
+      placeholder="Hasta"
+      :disabled="disabled"
+    />
+  </div>
 </template>
 
 <script setup>
-
-import DatePicker from 'primevue/datepicker';
-import { computed } from "vue";
+import { computed } from 'vue'
 import { useFallidasCtStore } from '../../store/CtFallidaStore'
+import CtDatePicker from '../CtDatePicker.vue'
+import './filter-element.css'
+
+defineProps({
+  disabled: { type: Boolean, default: false }
+})
 
 const store = useFallidasCtStore()
+
 const fechaHasta = computed({
   get: () => store.filters.fechaCierreOTHasta,
-  set: (fechaHasta) =>
-    store.setFilter('fechaCierreOTHasta', fechaHasta? fechaHasta : '') 
+  set: (value) => store.setFilter('fechaCierreOTHasta', value ?? null)
 })
-const borrarFecha = (event) => {
-    store.setFilter('fechaCierreOTHasta',null)
-}
-
-
 </script>
-
-<style scoped>
-</style>
